e8035ed81478d763627675ca46af56d3155c419b
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-08-03 02:58:45 +0200

    fix: foo

--- web-ext.config.ts
+++ web-ext.config.ts
@@ -2,6 +2,6 @@ import { defineWebExtConfig } from 'wxt';
 
 export default defineWebExtConfig({
   chromiumArgs: ['https://www.werstreamt.es/filme-serien/liste-411079'],
-  chromiumProfile: 'userdata', // chromiumArgs: ['--user-data-dir=userdata'] doesn't keep sessions across dev restarts
+  chromiumProfile: 'userdata', // chromiumArgs: ['--user-data-dir=userdata'] doesn't keep sessions across dev restarts,
   keepProfileChanges: true,
 });
