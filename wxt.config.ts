907daa2ee46028ba25c7ad99939c5125b073d2e6
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-12 23:40:23 +0100

    fix: foo

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'wxt';
 
 export default defineConfig({
+  autoIcons: { developmentIndicator: false },
   manifest: { name: 'WerStreamt.es Listen Links' },
+  modules: ['@wxt-dev/auto-icons'],
 });
